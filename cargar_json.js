c2cce8414c801bd6126203cbeb493dfb66f1c1bd
Taylor Bundy <martonbarbosa@hotmail.com>
2025-10-15 21:56:48 -0300

    gogogo

--- cargar_json.js
+++ cargar_json.js
@@ -30,7 +30,8 @@ async function cargarJSON() {
         // Llamamos a mostrarTabla con los datos adaptados
         mostrarTabla(datos);
         console.log(datos);
-        localStorage.setItem('pruebaonline', datos);
+        //localStorage.setItem('pruebaonline', datos);
+        localStorage.setItem('pruebaonline', JSON.stringify(datos));
 
         return datos; // opcional, si necesitas usar los datos luego
     } catch (err) {
